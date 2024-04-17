05642123fb51414a20561802ea3da6ec625db3aa
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-04-17 14:45:55 +0100

    comment hostname prop

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  // hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
   author: {
     name: "Mr.Hope",
